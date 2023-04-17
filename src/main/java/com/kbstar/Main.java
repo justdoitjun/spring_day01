418347b8ac2d1da4a18fbc0e3a6c4a3318236af3
junstdoitjun <bing74700@gmail.com>
2023-04-17 15:49:42 +0900

    test

--- src/main/java/com/kbstar/Main.java
+++ src/main/java/com/kbstar/Main.java
@@ -4,6 +4,7 @@ package com.kbstar;public class Main {
         int a = 10;
         int b = 20;
         int c = 30;
+        Service service = new Service();
         for (int i =1; i <=5; i++){
             System.out.println("i=" + i);
         }
